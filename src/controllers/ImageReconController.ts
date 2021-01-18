973fb0a54cc8f11a20a7416571f4d1b6c2019e45
escobarlep <lucasescperes@gmail.com>
2021-01-17 23:11:08 -0300

    feat: create compare methods

--- src/controllers/ImageReconController.ts
+++ src/controllers/ImageReconController.ts
@@ -2,12 +2,13 @@ import { ImageReconService } from '../services/ImageReconService'
 import { imageToBase64 } from '../helpers/index'
 import { Request, Response } from 'express'
 import { CompareService } from '../services/CompareService'
+import Logger from '../helpers/Logger'
+const log = new Logger('controller:ImageReconController')
 
 export class ImageReconController {
 
   public async read(req: Request, res: Response) {
     try {
-      //const { image } = req.body;
       const image = imageToBase64('src/resources/cnh-escobar.jpg')
       const irService = new ImageReconService()
       const result = await irService.read(image)
@@ -15,7 +16,7 @@ export class ImageReconController {
 
       return res.json(parsedAnnotations)
     } catch (error) {
-      console.log(error)
+      log.err('error', error)
       return res.json({msg: 'error', error: error.message})  
     }
   }
@@ -23,13 +24,18 @@ export class ImageReconController {
   public async compare(req: Request, res: Response) {
     try {
       const { base64Image, data } = req.body;
+
       const irService = new ImageReconService()
-      const compareService = new CompareService()
       const result = await irService.read(base64Image)
       const parsedAnnotations = irService.parse(result)
-  
-      return res.json(compareService.compare(parsedAnnotations, data)) 
+
+      const compareService = new CompareService(parsedAnnotations)
+      const dataCompared = compareService.compare(data)
+
+      return res.json(dataCompared)
     } catch (error) {
+      log.err('error', error)
+
       return res.json(error)
     }
   }
